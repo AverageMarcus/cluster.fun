c65725b41fb4a814cd247e8ded9039b0c5b565d8
Marcus Noble <github@marcusnoble.co.uk>
2020-05-02 10:58:55 +0100

    Updated kubernetes cluster terraform

--- terraform/kubernetes-cluster.tf
+++ terraform/kubernetes-cluster.tf
@@ -6,15 +6,6 @@ resource "scaleway_k8s_cluster_beta" "k8s-cluster" {
   enable_dashboard = false
   ingress          = "traefik"
 
-  default_pool {
-    node_type   = "DEV1-M"
-    size        = 1
-    autoscaling = true
-    autohealing = true
-    min_size    = 1
-    max_size    = 2
-  }
-
   feature_gates = [
     "EphemeralContainers",
     "HPAScaleToZero",
@@ -22,6 +13,17 @@ resource "scaleway_k8s_cluster_beta" "k8s-cluster" {
   ]
 }
 
+
+resource "scaleway_k8s_pool_beta" "k8s-cluster-pool-1" {
+  cluster_id = scaleway_k8s_cluster_beta.k8s-cluster.id
+  name = "node pool 1"
+  node_type = "DEV1-M"
+  size = 1
+  min_size = 1
+  max_size = 2
+  autoscaling = true
+  autohealing = true
+}
 
 output "api_server" {
   value = scaleway_k8s_cluster_beta.k8s-cluster.apiserver_url
