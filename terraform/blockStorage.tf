dfc6f23eba7800dbadf649412e82ad2b54d4b65c
Marcus Noble <github@marcusnoble.co.uk>
2020-05-02 21:07:45 +0100

    Removed unneeded block storage TF

--- terraform/blockStorage.tf
+++
@@ -1,9 +0,0 @@
-# resource "scaleway_instance_volume" "kubernetes" {
-#     type       = "l_ssd"
-#     name       = "kubernetes"
-#     size_in_gb = 100
-# }
-
-# output "block-storage-id" {
-#   value = scaleway_instance_volume.kubernetes.id
-# }
