0f58b53e4430d84c2f5ac62bbfd8c8b21a713994
Marcus Noble <github@marcusnoble.co.uk>
2020-05-25 14:20:46 +0100

    Added linx-server

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -6,3 +6,12 @@ resource "scaleway_object_bucket" "kubernetes" {
 output "bucket_id" {
   value = scaleway_object_bucket.kubernetes.id
 }
+
+resource "scaleway_object_bucket" "linx" {
+  name = "cluster.fun-linx"
+  acl  = "private"
+}
+
+output "linx-bucket_id" {
+  value = scaleway_object_bucket.linx.id
+}
--- terraform/kubernetes-cluster.tf
+++ terraform/kubernetes-cluster.tf
@@ -17,7 +17,7 @@ resource "scaleway_k8s_pool_beta" "k8s-cluster-pool-1" {
   cluster_id  = scaleway_k8s_cluster_beta.k8s-cluster.id
   name        = "node pool 1"
   node_type   = "DEV1-M"
-  size        = 2
+  size        = 1
   min_size    = 1
   max_size    = 2
   autoscaling = true
