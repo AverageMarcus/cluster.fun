9f65bf256a384f040ce5e81e99b3ea938da0fd5a
Marcus Noble <github@marcusnoble.co.uk>
2020-08-23 11:16:02 +0100

    Added bucket for storing SCP archives

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -24,3 +24,12 @@ resource "scaleway_object_bucket" "outline" {
 output "outline-bucket_id" {
   value = scaleway_object_bucket.outline.id
 }
+
+resource "scaleway_object_bucket" "scp-archives" {
+  name = "scp-archives"
+  acl  = "public-read"
+}
+
+output "scp-bucket_id" {
+  value = scaleway_object_bucket.scp-archives.id
+}
