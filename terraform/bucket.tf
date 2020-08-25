cf2a889e4d568bf9151a8545d007e2a81a2fae31
Marcus Noble <github@marcusnoble.co.uk>
2020-08-25 15:25:01 +0100

    Removed SCP archives

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -24,12 +24,3 @@ resource "scaleway_object_bucket" "outline" {
 output "outline-bucket_id" {
   value = scaleway_object_bucket.outline.id
 }
-
-resource "scaleway_object_bucket" "scp-archives" {
-  name = "scp-archives"
-  acl  = "public-read"
-}
-
-output "scp-bucket_id" {
-  value = scaleway_object_bucket.scp-archives.id
-}
