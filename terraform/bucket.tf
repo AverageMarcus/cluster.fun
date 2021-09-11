0db4e321ea985f6732d605aa18a559d3c20b80bd
Marcus Noble <github@marcusnoble.co.uk>
2021-09-11 09:56:37 +0100

    Added outline

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -6,3 +6,12 @@ resource "scaleway_object_bucket" "kubernetes" {
 output "bucket_id" {
   value = scaleway_object_bucket.kubernetes.id
 }
+
+resource "scaleway_object_bucket" "outline" {
+  name = "cluster.fun-outline"
+  acl  = "private"
+}
+
+output "outline-bucket_id" {
+  value = scaleway_object_bucket.outline.id
+}
